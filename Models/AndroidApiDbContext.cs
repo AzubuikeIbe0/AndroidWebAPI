1133383ac1b516e86ea724339783256896a1d03a
ai-dev-official <ai.codespro@gmail.com>
2024-03-08 18:36:27 +0000

    Add project files.

---
+++ Models/AndroidApiDbContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AndroidWebAPI.Models;
+
+public partial class AndroidApiDbContext : DbContext
+{
+    public AndroidApiDbContext()
+    {
+    }
+
+    public AndroidApiDbContext(DbContextOptions<AndroidApiDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Article> Articles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:android-sql-server.database.windows.net;Database=AndroidApiDB;User ID=sql-server-root;Password=b33\"t2xVA!M~sed;Encrypt=True;TrustServerCertificate=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Article>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Article__3213E83FC85C2745");
+
+            entity.ToTable("Article");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("id");
+            entity.Property(e => e.Author)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("author");
+            entity.Property(e => e.AuthorImageUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("authorImageUrl");
+            entity.Property(e => e.Body)
+                .HasColumnType("text")
+                .HasColumnName("body");
+            entity.Property(e => e.Category)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("category");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("createdAt");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("imageUrl");
+            entity.Property(e => e.Subtitle)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("subtitle");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.Views).HasColumnName("views");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AndroidWebAPI.Models;
+
+public partial class Article
+{
+    public string Id { get; set; } = null!;
+
+    public string? Title { get; set; }
+
+    public string? Subtitle { get; set; }
+
+    public string? Body { get; set; }
+
+    public string? Author { get; set; }
+
+    public string? AuthorImageUrl { get; set; }
+
+    public string? Category { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public int? Views { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
